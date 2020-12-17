1435f66100bc07e78f0b23d60ecd3f8e8fed7095
kunxin-chor <chorkunxin@yahoo.com>
2020-12-17 04:25:32 +0000

    Added solution for handson 2.1

--- 02-components/src/App.js
+++ 02-components/src/App.js
@@ -8,11 +8,17 @@ function sayGoodbye() {
     return <h2>Time to say goodbye</h2>
 }
 
+function BorderedImageFrame(){
+    return (
+        <img style={{'border':'4px solid red'}} src={require('./pikachu.png').default}/>
+    )
+}
+
 // 1. is a function
 // 2. first alphabet is uppercase
 // 3. return JSX
 function ImageFrame(){
-    return <img src={require('./dog.gif').default}/>
+    return <img  src={require('./dog.gif').default}/>
 }
 
 function App() {
@@ -21,6 +27,7 @@ function App() {
         <h1>{sayHello()}</h1>
         {sayGoodbye()}
         <ImageFrame/>
+        <BorderedImageFrame/>
    </React.Fragment>
 
   );
