822a92c925991649c02d69cd51f613225caf58f5
kunxin-chor <chorkunxin@yahoo.com>
2020-12-17 08:00:09 +0000

    Added timer example

--- 03-states/src/App.js
+++ 03-states/src/App.js
@@ -1,9 +1,16 @@
 import NumberBox from './NumberBox'
+import ImageSwitcher from './ImageSwitcher'
+import Counter from './Counter'
+import Timer from './Timer'
 
 function App() {
   return (
     <div className="App">
+        <Timer/>
+        <Counter/>
         <NumberBox startingNumber={42}/>
+        <ImageSwitcher/>
+
     </div>
   );
 }
