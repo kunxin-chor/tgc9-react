7dad458d0412869a2b9da9dca243939049396c6d
kunxin-chor <chorkunxin@yahoo.com>
2020-12-30 03:33:38 +0000

    Added show listing page

--- 12-master-details/src/pages/ProductListing.js
+++ 12-master-details/src/pages/ProductListing.js
@@ -5,6 +5,35 @@ export default function ProductListing() {
     const context = useContext(ProductContext);
     return <React.Fragment>
         <h1>Product Listing</h1>
+        <table className="table">
+            <tr>
+                <th>
+                    SKU
+                </th>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Price
+                </th>
+            </tr>
+
+            {context.products.map(p=>(
+                <tr>
+                    <td>
+                        {p.sku}
+                    </td>
+                    <td>
+                        {p.name}
+                    </td>
+                    <td>
+                        {p.price}
+                    </td>
+                </tr>
+
+            ))}
+        </table>
+
     </React.Fragment>
 
 }
