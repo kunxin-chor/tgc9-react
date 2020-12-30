0b07aedb9156a896a53ea1310ecaf918f7ac7f16
kunxin-chor <chorkunxin@yahoo.com>
2020-12-30 03:43:23 +0000

    Gotten the add product form to work

--- 12-master-details/src/pages/AddProduct.js
+++ 12-master-details/src/pages/AddProduct.js
@@ -1,8 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function AddProduct() {
-    return <React.Fragment>
-        <h1>Add Product</h1>
-    </React.Fragment>
+  const [newProduct, setNewProduct] = useState({
+    sku: "",
+    name: "",
+    price: 0,
+    imageUrl: ""
+  });
 
+  const updateFormField = (event) => {
+     setNewProduct({
+          ...newProduct,
+          [event.target.name] : event.target.value
+      })
+  }
+
+  return (
+    <React.Fragment>
+      <h1>Add Product</h1>
+      <div>
+        <label className="form-label">SKU</label>
+        <input
+          type="text"
+          className="form-control"
+          name="sku"
+          value={newProduct.sku}
+          onChange={updateFormField}
+        />
+      </div>
+       <div>
+        <label className="form-label">Name</label>
+        <input
+          type="text"
+          className="form-control"
+          name="name"
+          value={newProduct.name}
+          onChange={updateFormField}
+        />
+      </div>
+       <div>
+        <label className="form-label">Price</label>
+        <input
+          type="text"
+          className="form-control"
+          name="price"
+          value={newProduct.price}
+          onChange={updateFormField}
+        />
+      </div>
+       <div>
+        <label className="form-label">Image URL</label>
+        <input
+          type="text"
+          className="form-control"
+          name="imageUrl"
+          value={newProduct.imageUrl}
+          onChange={updateFormField}
+        />
+      </div>
+    </React.Fragment>
+  );
 }
